d10ab04fd5e07253db1337f8efdfccadb436b919
Tim Muenzinger <muenzinger.tim@googlemail.com>
2020-11-03 11:37:02 +0100

    added component for current day data

--- src/app.js
+++ src/app.js
@@ -35,13 +35,13 @@ Vue.component("country-selection", {
                 <label for="country">{{ country }}</label>
             </div>
         </div>
-        <input class="submitButton" type="submit" value="Submit"></input>
+        <input class="submitButton" type="submit" value="Plot Charts"></input>
     </form> 
     `
 });
 
-// display graphs component
-Vue.component("graphs-section", {
+// display data component
+Vue.component("data-section", {
     props: {
         jsonData: {
             type: Object,
@@ -54,57 +54,143 @@ Vue.component("graphs-section", {
     },
     data: function() {
         return {
-            graphs: []
+
         }
     },
     computed: {
-       
+       updateDay: function() {
+            //return Object.values(this.jsonData[0].data)[this.jsonData[0].data.length - 1].date;
+       }
     },
-    watch: {
-        checkedCountries: function() {
-            this.plotCharts();
+    template: `
+    <div>
+        <p>Data is updatet daylie around 11:00 UTC+1 (06:00 AM EST).</p>
+        <p>Data was last updatet: {{ updateDay }}</p>
+        <div v-for="country in checkedCountries">
+            <current-day-data :jsonData="jsonData"
+                :country="country"></current-day-data>
+            <chart></chart>
+        </div>
+    </div>
+    `
+});
+
+Vue.component("current-day-data", {
+    props: {
+        jsonData: {
+            type: Object,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
         }
+    },
+    data: function() {
+        return {
+            newCases: 0,
+            totalCases: 0,
+            newDeaths: 0,
+            totalDeaths: 0,
+            newCasesPerMillion: 0,
+            totalCasesPerMillion: 0,
+            newDeathsPerMillion: 0,
+            totalDeatchsPerMillion: 0,
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        currentDateData: function() {
+
+            const THIS = this;
+
+            var today = new Date();
+            var yesterday = new Date(today);
+            yesterday.setDate(yesterday.getDate() - 1);
+            today = today.toISOString().slice(0, 10);
+            yesterday = yesterday.toISOString().slice(0, 10);
+
+            var countryData;
+
+            // get single country object for plotting
+            for(var i = 0; i < Object.keys(jsonData).length; i++) {
+
+                if(Object.values(jsonData)[i].location == this.country) {
+
+                    countryData = Object.values(jsonData)[i];
+                }
+            }
+
+            // todays data or if not yet updatet yesterdays data
+            for(var index = 0; index < countryData.data.length; index++) {
+
+                if((Object.values(countryData.data)[index].date == today) 
+                    || (Object.values(countryData.data)[index].date == yesterday 
+                    && !Object.values(countryData.data)[index + 1])) {  
+
+                    THIS.newCases = Object.values(countryData.data)[index].new_cases;
+                    THIS.totalCases = Object.values(countryData.data)[index].total_cases;
+                    THIS.newDeaths = Object.values(countryData.data)[index].new_deaths;
+                    THIS.totalDeaths = Object.values(countryData.data)[index].total_deaths;
+                    THIS.newCasesPerMillion = Object.values(countryData.data)[index].new_cases_per_million;
+                    THIS.totalCasesPerMillion = Object.values(countryData.data)[index].total_cases_per_million;
+                    THIS.newDeathsPerMillion = Object.values(countryData.data)[index].new_deaths_per_million;
+                    THIS.totalDeatchsPerMillion = Object.values(countryData.data)[index].total_deaths_per_million;
+                }
+            }
+        }
+    },
+    mounted: function() {
+        this.currentDateData();
+    },
+    template: `
+    <div>
+        <p>{{ this.country }}</p>
+        <p>New cases: {{ this.newCases }}</p>
+        <p>Total cases: {{ this.totalCases }}</p>
+        <p>New Deaths: {{ this.newDeaths }}</p>
+        <p>Total Deaths: {{ this.totalDeaths }}</p>
+        <p>New cases per million: {{ this.newCasesPerMillion }}</p>
+        <p>Total cases per million: {{ this.totalCasesPerMillion }}</p>
+        <p>New deaths per million: {{ this.newDeathsPerMillion }}</p>
+        <p>Total cases per million: {{ this.totalDeatchsPerMillion }}</p>
+    </div>
+    `
+});
+
+Vue.component("chart", {
+    props: {
+
+    },
+    data: function() {
+        return {
+            
+        }
+    },
+    computed: {
+
     },
     methods: {
         plotCharts: function() {
 
-            var charts = [];
-        
+
+
             // plot all charts
-            this.checkedCountries.forEach(function(countryName) {
+
                 
-                var dataToPlot;
-
-                // get single object to plot
-                this.jsonDataforEach(function(countryObject) {
-                    
-                    if(countryObject.location == countryName) {
-                        dataToPlot = countryObject;
-                    }
-                });
-
-                // todo
-
                 // pre process data for plotting
 
-                // plot single chart
-                // from current day data
- 
-                // set context (canvasId) -> dynamic id
-                var context = document.getElementById().getContext("2d");
-                var chart = new Chart(context, {
-                    type: "line"
-                });
+                
+    
+                // plot charts
 
-
-                charts.push(chart);
-            });
+      
         }
     },
     template: `
-    <div>
-        <p>{{ checkedCountries }}</p>
-    </div>
+
     `
 });
 
@@ -143,6 +229,7 @@ var app = new Vue({
 /** 
  * todo:
  * plot and display charts
- * check if new data available and cach json (>40mb file) 
+ * check if new data available and cach json (>40mb file)
+ * CLI 
  * styling 
  */
